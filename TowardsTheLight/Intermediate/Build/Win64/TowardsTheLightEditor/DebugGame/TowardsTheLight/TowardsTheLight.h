2f98c228c2a9526f3cede8b2aa3ddd981536b617
Manuel Cruz Ramírez <manuel.cruz@live.u-tad.com>
2015-04-21 13:21:33 +0200

    cambios en el gitignore

--- TowardsTheLight/Intermediate/Build/Win64/TowardsTheLightEditor/DebugGame/TowardsTheLight/TowardsTheLight.h
+++
@@ -1,5 +0,0 @@
-#ifndef __AUTO_TOWARDSTHELIGHT_H__
-#define __AUTO_TOWARDSTHELIGHT_H__
-//Last Write: 20/04/2015 14:56:14 +00:00
-#include "C:\Users\alfredo.sosa\Downloads\TowardTheLight\TowardsTheLight\Source\TowardsTheLight\TowardsTheLight.h"
-#endif//__AUTO_TOWARDSTHELIGHT_H__
--- TowardsTheLight/Intermediate/Build/Win64/TowardsTheLightEditor/Development/TowardsTheLight/PCH.TowardsTheLight.TowardsTheLight.h.cpp
+++
@@ -1,1 +0,0 @@
-#include "D:\master_videojuegos\proyecto\TowardTheLight\TowardsTheLight\Source\TowardsTheLight\TowardsTheLight.h"
