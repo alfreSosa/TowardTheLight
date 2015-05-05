304dc3de5f57418a8f24a55936ceb4048b079b70
Manuel Cruz Ram�rez <manuel.cruz@live.u-tad.com>
2015-05-05 23:52:11 +0200

    reseteo prueba del gitignore

--- Source/TowardsTheLight/TowardsTheLight.cpp
+++
@@ -1,5 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "TowardsTheLight.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TowardsTheLight, "TowardsTheLight" );
