a7ece7417269e00d0d01afc5b03aa9de8f75d1b3
Alfredo Sosa <alfredo.sosa@WKSALMPBB473.u-tad.es>
2015-05-11 17:46:23 +0200

    Empezando objectos pickable. Se encuentran en desarrollo.

---
+++ Source/TowardsTheLight/PickableItem.h
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "PickableItem.generated.h"
+
+UCLASS()
+class TOWARDSTHELIGHT_API APickableItem : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	APickableItem();
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void Tick( float DeltaSeconds ) override;
+  virtual void ReceiveActorBeginOverlap(AActor* OtherActor) override;
+
+  UPROPERTY()
+    USceneComponent* OurVisibleComponent;
+	
+};
