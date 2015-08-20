7953ff82e6564313f1635908b3b6e4b4514bd586
Manuel Cruz Ram�rez <manuel.cruz@live.u-tad.com>
2015-08-20 11:18:11 +0200

    feedback visual en el báculo

--- Source/TowardsTheLight/Stick.cpp
+++ Source/TowardsTheLight/Stick.cpp
@@ -4,17 +4,15 @@
 #include "Stick.h"
 
 
-AStick::AStick()
-{
+AStick::AStick(){
   RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("RootComponent"));
   Stick = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("OurVisibleComponent"));
   Stick->SetMobility(EComponentMobility::Movable);
   Stick->CastShadow = false;
   Stick->AttachTo(RootComponent);
-  UStaticMesh *mesh;
+  UStaticMesh *mesh = nullptr;
   static ConstructorHelpers::FObjectFinder<UStaticMesh> MeshFinder(TEXT("StaticMesh'/Game/Models/Baculo/baculo_model.baculo_model'"));
-  if (MeshFinder.Succeeded())
-  {
+  if (MeshFinder.Succeeded()){
     mesh = MeshFinder.Object;
     Stick->SetStaticMesh(mesh);
   }
@@ -22,20 +20,41 @@ AStick::AStick()
   StickMaterial = ((UPrimitiveComponent*)GetRootComponent())->CreateAndSetMaterialInstanceDynamic(0);
   UMaterial* mat = nullptr;
   static ConstructorHelpers::FObjectFinder<UMaterial> MatFinder(TEXT("Material'/Game/Models/Baculo/baculo_diffuse.baculo_diffuse'"));
-  if (MatFinder.Succeeded())
-  {
+  if (MatFinder.Succeeded()){
     mat = MatFinder.Object;
     StickMaterial = UMaterialInstanceDynamic::Create(mat, GetWorld());
   }
+
+  EffectsMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshEffects"));
+  EffectsMesh->AttachTo(RootComponent);
+  EffectsMesh->SetMobility(EComponentMobility::Movable);
+  EffectsMesh->CastShadow = false;
+  mesh = nullptr;
+  static ConstructorHelpers::FObjectFinder<UStaticMesh> MeshFinderEffects(TEXT("StaticMesh'/Game/Models/Baculo/baculoBloom_mesh.baculoBloom_mesh'"));
+  if (MeshFinderEffects.Succeeded()){
+    mesh = MeshFinderEffects.Object;
+    EffectsMesh->SetStaticMesh(mesh);
+  }
+  EffectsMesh->SetRelativeRotation(FRotator::MakeFromEuler(FVector(0, 0, 90)));
+  EffectsMesh->SetRelativeLocation(FVector(0, 0, 70));
+  EffectsMesh->SetWorldScale3D(FVector(0.75, 0.75, 0.75));
+  EffectsMaterial = ((UPrimitiveComponent*)GetRootComponent())->CreateAndSetMaterialInstanceDynamic(1);
+  mat = nullptr;
+  static ConstructorHelpers::FObjectFinder<UMaterial> MatFinderEffects(TEXT("Material'/Game/Models/Baculo/baculoBloom_material.baculoBloom_material'"));
+  if (MatFinderEffects.Succeeded()){
+    mat = MatFinderEffects.Object;
+    EffectsMaterial = UMaterialInstanceDynamic::Create(mat, GetWorld());
+  }
 }
 
-void AStick::BeginPlay()
-{
+void AStick::BeginPlay(){
 	Super::BeginPlay();
   Stick->SetMaterial(0, StickMaterial);
+  EffectsMesh->SetMaterial(0, EffectsMaterial);
 }
 
 void AStick::SetColor(FLinearColor colorKey, float shinnes) {
   StickMaterial->SetScalarParameterValue("Brillo", shinnes);
   StickMaterial->SetVectorParameterValue("BaculoColor", colorKey);
+  EffectsMaterial->SetVectorParameterValue("Bloom_Color", colorKey);
 }
